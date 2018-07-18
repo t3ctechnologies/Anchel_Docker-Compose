43e2daf051c6264b1fb3ab0851dd72dfcae500cd
Dayanand <dayanand.k@shrigowri.com>
2018-07-18 14:32:45 +0530

    updated

--- conf/home/js/style.js
+++ conf/home/js/style.js
@@ -16,3 +16,30 @@ $(document).ready(function() {
         });
     });
 })
+
+/* $(document).ready(function(){
+    
+    $("#Layer5 a").on('click', function(event) {
+      
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+        window.location.hash = hash;
+        });
+      }  
+    });
+}); */
+$(window).on('load', function(){
+    $("#Layer5 a[href^='#']").on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 60
+          }, 1000, function(){
+              /* window.location.hash = hash; */
+          });
+     });
+    });
